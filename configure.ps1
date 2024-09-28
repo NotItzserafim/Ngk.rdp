ddc6c3094e63f31146c086d6e5ec090881c3e3cc
Serafim D <Hey@itzserafim.nl>
2024-09-29 00:42:07 +0200

    Update configure.ps1

--- configure.ps1
+++ configure.ps1
@@ -3,14 +3,6 @@
 # This script is executed when GitHub actions is initialized.
 Write-Output "[INFO] Script started!"
 
-# First we download ngrok
-Invoke-WebRequest -Uri https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-windows-amd64.zip -OutFile ngrok.zip
-
-# Then we unzip it
-Expand-Archive -LiteralPath '.\ngrok.zip'
-
-# Set-up and save ngrok authtoken
-./ngrok/ngrok.exe authtoken $env:NGROK_AUTH_TOKEN
 
 # Haal alle netwerkinterfaces op
 $interfaces = Get-NetAdapter | Where-Object { $_.Status -eq 'Up' }
