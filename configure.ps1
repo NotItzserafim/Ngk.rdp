5f5d9202911fdbc8eaa371a320abe4019a1a971b
Serafim D <Hey@itzserafim.nl>
2024-09-29 00:45:36 +0200

    Update configure.ps1

--- configure.ps1
+++ configure.ps1
@@ -3,6 +3,20 @@
 # This script is executed when GitHub actions is initialized.
 Write-Output "[INFO] Script started!"
 
+# First we download ngrok
+Invoke-WebRequest -Uri https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-windows-amd64.zip -OutFile ngrok.zip
+
+# Then we unzip it
+Expand-Archive -LiteralPath '.\ngrok.zip'
+
+# Set-up and save ngrok authtoken
+./ngrok/ngrok.exe authtoken $env:NGROK_AUTH_TOKEN
+
+# Start ngrok tunnel for RDP (assuming RDP runs on port 3389)
+Start-Process -NoNewWindow -FilePath './ngrok/ngrok.exe' -ArgumentList 'tcp 3389'
+
+# Wacht een paar seconden om ervoor te zorgen dat ngrok opstart
+Start-Sleep -Seconds 5
 
 # Haal alle netwerkinterfaces op
 $interfaces = Get-NetAdapter | Where-Object { $_.Status -eq 'Up' }
@@ -16,18 +30,15 @@ foreach ($interface in $interfaces) {
 Get-DnsClientServerAddress
 
 # Login op AD domein
-netdom.exe join %COMPUTERNAME% /domain:media.itzserafim.nl /UserD:MEDIAITZSERAFIM\serafimdy /PasswordD:$env:PASSWORD_AD
-
-
-
-
+netdom.exe join $env:COMPUTERNAME /domain:media.itzserafim.nl /UserD:MEDIAITZSERAFIM\serafimdy /PasswordD:$env:PASSWORD_AD
 
 # Enabling RDP Access
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name "fDenyTSConnections" -Value 0
 
 # Authorize RDP Service from firewall so we don't get a weird state
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
 # Change password to the one we set-up as RDP_PASSWORD on our repo settings.
 Set-LocalUser -Name "runneradmin" -Password (ConvertTo-SecureString -AsPlainText "$env:RDP_PASSWORD" -Force)
+
 Exit
